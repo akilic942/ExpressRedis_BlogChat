897641bef47bd9c04e5c9c577ad26a7b689084ed
aylinmengi <aylinmengi@ext-40-187.eduroam.rwth-aachen.de>
2015-05-16 18:44:35 +0200

    test

---
+++ A2.js
@@ -0,0 +1,4 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();     // Teil des bodyParsers.. gibt Parser zurück der mit json Datein arbeitet
+var A2 = express();
