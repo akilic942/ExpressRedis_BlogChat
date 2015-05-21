5cf1645110ed0c96b126f1088a5f671c98884a95
kaneda942 <kaneda942@gmail.com>
2015-05-21 16:16:46 +0200

    Switch Case in get:/rezepte/:id für accepted abfrage html/json. DEFAULT-Case funktioniert nicht, gibt loop.

--- index.js
+++ index.js
@@ -10,29 +10,41 @@ var datadir = __dirname + '/DATA/';
 
 
 app.get('/', function(req, res){
-   res.status(200).send('Hallo! Das ist die Aktuelle Startseite unseres Projekts') ; 
+   
+    res.status(200).send('Hallo! Das ist die Aktuelle Startseite unseres Projekts') ; 
 });
+
 
 app.post('/rezepte', jsonParser, function(req, res){
 });
 
+
 app.get('/rezepte/:id', function(req, res){
+   
     var id = req.params.id;
     var objid = id % 20;
     var page = ~~(id/20);
     
-    fs.readFile(datadir + page +'.json', function(err, data){
-    if(err) throw err;
-    else {
-        var data = JSON.parse(data); 
-        res.send("Name:" + data[objid].name + "\n" + "Autor:" + data[objid].autor + "\n" + "Zutaten:" + data[objid].zutaten + "\n" + "Rezept:" + data[objid].rezept);
-
-        }
+    fs.readFile(datadir +'rezepte/'+ page +'.json', function(err, data){
+        if(err) throw err;
+        else {
+            var data = JSON.parse(data); 
+            var acceptedTypes =req.accepts(['html', 'json']);
         
-         
+            switch(acceptedTypes){
+                case 'html':
+                    res.send("Name:" + data[objid].name + "\n" + "Autor:" + data[objid].autor + "\n" + "Zutaten:" + data[objid].zutaten + "\n" + "Rezept:" + data[objid].rezept).status(200);
+                    break;
+                case 'json':
+                    res.json(data[objid]).status(200);
+                    break;
+                default:
+                    res.status(406).end;
+                    break;
+            }
+        }         
     });
-  }
-);
+});
 
 
 app.delete('/rezepte/:id', function(req, res){
