bce5fd76624e08db0accdaa33825af2581ee38eb
kaneda942 <kaneda942@gmail.com>
2015-05-25 03:38:33 +0200

    Von Neu

--- index.js
+++ index.js
@@ -1,88 +1,28 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var fs = require('fs');
+var redis = require('redis');
 
+var db = redis.createClient();
 var app = express();
-var jsonParser = bodyParser.json();     
 
-var dataDir = __dirname + '/DATA/';
-var dataLimit = 20;
+app.use(bodyParser.json());
 
+//Posten eines Rezepts mit ID (nach WorkshopBeispiel) //
+app.post('/rezept', function(req,res){
 
+  var newPost = req.body;
 
-app.get('/', function(req, res){
-   
-    res.status(200).send('Hallo! Das ist die Aktuelle Startseite unseres Projekts') ; 
-});
+  db.incr('id:rezept', function(err,rep){
 
+    newPost.id = rep;
 
-app.get('/rezepte/', function(req,res){
-    var rezepte = dataDir + 'rezepte/'
-    
-    if(req.query.name !== undefined){
-        fs.readdir(rezepte,function(err,files){
-                if (err) throw err;
-                var c=0;
-                
-            files.forEach(function(file){
-                    c++;
-                
-                    fs.readFile(rezepte + file,function(err,data){
-                        if (err) throw err;
-                        var data = JSON.parse(data);
-                        data[file]=data;
-                        
-                        res.end(JSON.stringify(data[file].filter(function(e, i, arr){
-                        return e.name.toLowerCase() == req.query.name.toLowerCase();
-                        })));
-                    });
-                });
-            });
-    }
-    
+    db.set('Rezept:' +newPost.id , JSON.stringify(newPost), function(err,rep){
+      res.json(newUser);
+    });
+
+  });
 
 });
 
-app.get('/rezepte/:id', function(req, res){
-   
-    var id = req.params.id;
-    var objid = id % dataLimit;
-    var page = ~~(id/dataLimit);
-    var rezepte = dataDir + 'rezepte/'
-    
-        fs.readFile(dataDir +'rezepte/'+ page +'.json', function(err, data){
-            if(err) throw err;
-            else {
-                var data = JSON.parse(data); 
-                var acceptedTypes =req.accepts(['html', 'json']);
-        
-                switch(acceptedTypes){
-                    case 'html':
-                        res.send("Name:" + data[objid].name + "\n" + "Autor:" + data[objid].autor + "\n" + "Zutaten:" + data[objid].zutaten + "\n" + "Rezept:" + data[objid].rezept).status(200);
-                        break;
-                    case 'json':
-                        res.json(data[objid]).status(200);
-                        break;
-                    default:
-                        res.status(406).end;
-                        break;
-                }
-            }
-        });
-});
-
-
-app.post('/rezepte', jsonParser, function(req, res){
-});
-
-
-app.delete('/rezepte/:id', function(req, res){
-           fs.readFile(dataDir + id +'.json', function(err, data){
-            if(err) throw err;
-            else{
-            var data =JSON.parse(data);}
-           });
-});
-
-
-app.listen(3080,"localhost");
+// localhost:3080 //
+app.listen('3030');
