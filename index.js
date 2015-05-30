c6669ca35314c90d65f08ca3af72db117ad43eb5
kaneda942 <kaneda942@gmail.com>
2015-05-30 22:40:33 +0200

    Kommentar Löschfunktion eingefügt.
    Überprüft ob Kommentar überhaupt verfügbar.

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// Unveränderte Beispiele aus dem Workshop - 1, 2, 3
-// 1 Posten eines Post mit ID (nach WorkshopBeispiel)
+//-------------------------------------------------------
+// Post Funktion
+//-------------------------------------------------------
+// Posten eines Post mit ID (nach WorkshopBeispiel)
 app.post('/post', function(req, res){
 
   var newPost = req.body;
 
-  db.incr('ID:post', function(err, rep){
+  db.incr('Counter:PostsOverall', function(err, rep){
 
     newPost.id = rep;
 
@@ -25,40 +27,7 @@ app.post('/post', function(req, res){
 
 });
 
-// Posten eines Kommentares zum jeweilgen Post über HASH-Keys, ermöglicht das löschen und abrufen nach ID
-
-app.post('/post/:postid/comment/', function(req, res){
-
-  var newComment = req.body;
-
-  db.exists('Post:'+req.params.postid, function(err, rep){
-
-    if(rep){
-      db.incr('counter:CommentsOnPost'+req.params.postid, function(err, rep){
-          newComment.id = rep;
-
-        db.sadd('comments:post:'+req.params.postid, newComment.id,function(err, rep){
-
-          db.hset('comments:'+req.params.postid,newComment.id,JSON.stringify(newComment), function(err, rep){
-                  res.json(newComment);
-          });
-        });
-      });
-    }
-
-    else res.status(404).type('text').send('Es existiert kein Post, zudem sie ein Kommentar schreiben möchten');
-
-  });
-});
-
-
-// 2 Anfragen aller Post //
-app.get('/post/', function(err, rep){
-
-
-});
-
-// 3 Anfragen eines Post nach ID //
+// Anfragen eines Post nach ID //
 app.get('/post/:id', function(req, res){
 
   db.get('Post:'+req.params.id, function(err, rep){
@@ -70,6 +39,63 @@ app.get('/post/:id', function(req, res){
     }
   });
 });
+
+//-------------------------------------------------------
+// Kommentar Funktion
+//-------------------------------------------------------
+// Posten eines Kommentares zum jeweilgen Post über HASH-Keys, ermöglicht das löschen und abrufen nach ID
+
+app.post('/post/:postid/comment/', function(req, res){
+
+  var newComment = req.body;
+
+  db.exists('Post:'+req.params.postid, function(err, rep){
+
+    if(rep){
+      db.incr('CounterComments:OnPost'+req.params.postid, function(err, rep){
+        cid = rep;
+
+        db.sadd('comments:post:'+req.params.postid, cid,function(err, rep){
+
+          db.hset('comments:'+req.params.postid, cid, JSON.stringify(newComment), function(err, rep){
+                  res.json(newComment);
+          });
+        });
+      });
+    }
+
+    else res.status(404).type('text').send('Es existiert kein Post, zudem sie ein Kommentar schreiben möchten');
+
+  });
+});
+
+// Anfragen aller Kommentare zum Post
+app.get('/post/:id/comment/', function(req, res){
+  db.hgetall('comments:'+req.params.id, function(err, rep){
+    if(rep){
+      res.type('json').send(rep);
+    }
+
+    else {
+      res.status(404).type('text').send('Keine kommentare verfügbar')
+    }
+  })
+});
+
+//Löschen eines Kommentare :cid aus dem Post :pid
+app.delete('/post/:pid/comment/:cid',function(req, res){
+
+    db.SREM('comments:post:'+req.params.pid, req.params.cid, function(err, rep){
+      if(rep==0)res.status(400).type('text').send('klapptnicht');
+      else{
+        db.HDEL('comments:'+req.params.pid, req.params.cid, function(err, rep){
+        res.status(200).type('text').send('Erfolgreich gelöscht!')
+    });
+    }
+  });
+});
+
+//-------------------------------------------------------
 
 // localhost:3000 //
 app.listen('3000');
