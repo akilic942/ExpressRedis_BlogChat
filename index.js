1e1ddbfd208e3fcb6af0c063e2cf8c2c50566e57
kaneda942 <kaneda942@gmail.com>
2015-05-30 15:57:04 +0200

    [] Überarbeitung der Kommentierfunktion:
    Folgendes wurde erarbetet durch
    http://stackoverflow.com/questions/11614408/comment-system-with-redis-use-lists
    
    Kommentare werden nun nicht mehr über lpush geschrieben, da hier die Möglichkeit fehlt, einen Kommentar nach seiner ID nach zu löschen.
    Ermöglicht wird das jetzt über eine HASK-Key Tabelle.
    Zu jedem Post gehört eine SADD-Liste mit den IDs der Kommentare die im jeweiligen Post enthalten sind, über die jeweilige ID ist der Inhalt abrufbar.
    
    Mit
    >HGETALL comments:1
    erhalte ich alle Kommentare zum Post ID:1
    
    Löschen eines Kommentares:
    >SREM Comments:post:1 1
    >HDEL Comments:1 1

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ var app = express();
 
 app.use(bodyParser.json());
 
-// Unveränderte Beispiele aus dem Workshop - 1, 2, 3 //
-// 1 Posten eines Rezepts mit ID (nach WorkshopBeispiel) //
-app.post('/rezept', function(req, res){
+// Unveränderte Beispiele aus dem Workshop - 1, 2, 3
+// 1 Posten eines Post mit ID (nach WorkshopBeispiel)
+app.post('/post', function(req, res){
 
   var newPost = req.body;
 
-  db.incr('ID:rezept', function(err, rep){
+  db.incr('ID:post', function(err, rep){
 
     newPost.id = rep;
 
-    db.set('Rezept:'+newPost.id, JSON.stringify(newPost), function(err, rep){
+    db.set('Post:'+newPost.id, JSON.stringify(newPost), function(err, rep){
       res.json(newPost);
     });
 
@@ -25,39 +25,43 @@ app.post('/rezept', function(req, res){
 
 });
 
-// Posten eines Kommentares zum jeweilgen Post //
+// Posten eines Kommentares zum jeweilgen Post über HASH-Keys, ermöglicht das löschen und abrufen nach ID
 
-app.post('/rezept/:postid/comment/', function(req, res){
+app.post('/post/:postid/comment/', function(req, res){
 
   var newComment = req.body;
 
-  db.exists('Rezept:'+req.params.postid, function(err, rep){
+  db.exists('Post:'+req.params.postid, function(err, rep){
 
     if(rep){
-      db.incr('ID_comment:'+req.params.postid, function(err, rep){
-        newComment.id = rep;
+      db.incr('counter:CommentsOnPost'+req.params.postid, function(err, rep){
+          newComment.id = rep;
 
-        db.lpush('Kommentare zu Rezept:'+req.params.postid, JSON.stringify(newComment), function(err, rep){
-          res.json(newComment);
+        db.sadd('comments:post:'+req.params.postid, newComment.id,function(err, rep){
+
+          db.hset('comments:'+req.params.postid,newComment.id,JSON.stringify(newComment), function(err, rep){
+                  res.json(newComment);
+          });
         });
       });
     }
 
-    else res.status(404).type('text').send('Es existiert kein Rezept, zudem sie ein Kommentar schreiben möchten');
+    else res.status(404).type('text').send('Es existiert kein Post, zudem sie ein Kommentar schreiben möchten');
+
   });
 });
 
 
-// 2 Anfragen aller Rezepte //
-app.get('/rezept/', function(err, rep){
+// 2 Anfragen aller Post //
+app.get('/post/', function(err, rep){
 
 
 });
 
-// 3 Anfragen eines Rezepts nach ID //
-app.get('/rezept/:id', function(req, res){
+// 3 Anfragen eines Post nach ID //
+app.get('/post/:id', function(req, res){
 
-  db.get('Rezept:'+req.params.id, function(err, rep){
+  db.get('Post:'+req.params.id, function(err, rep){
     if(rep){
       res.type('json').send(rep);
     }
