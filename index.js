25c294ccb097ba11b60ff6f8d3ee8eaac65434f6
kaneda942 <kaneda942@gmail.com>
2015-06-04 16:25:56 +0200

    Einfügen von Countern:
    Counter:OnPostCOMMENTS - Menge an Kommentaren bei jeweiligen Post
    Counter:OnPostGET - Menge an GET-Requests bei jeweiligen Post
    Counter:OverallGET - Gesamte Menge an GET-Requests von Posts
    Counter:POSTS - Gesamte Menge an Posts

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var redis = require('redis');
+var moment = require('moment');
 
 var db = redis.createClient();
 var app = express();
+var date = moment().format('DD.MM.YYYY, HH:mm');
 
 app.use(bodyParser.json());
 
@@ -15,9 +17,10 @@ app.post('/post', function(req, res){
 
   var newPost = req.body;
 
-  db.incr('Counter:PostsOverall', function(err, rep){
+  db.incr('Counter:OverallPOSTS', function(err, rep){
 
     newPost.id = rep;
+    newPost.ErstelltAm = date;
 
     db.set('Post:'+newPost.id, JSON.stringify(newPost), function(err, rep){
       res.json(newPost);
@@ -29,15 +32,51 @@ app.post('/post', function(req, res){
 
 // Anfragen eines Post nach ID //
 app.get('/post/:id', function(req, res){
-
   db.get('Post:'+req.params.id, function(err, rep){
     if(rep){
-      res.type('json').send(rep);
+      var Post = rep;
+      db.incr('Counter:OverallGET');
+      db.zincrby('Counter:OnPostGET',1,'Post:'+req.params.id);
+      res.type('json').send(Post);
+
     }
     else {
       res.status(404).type('text').send('Diese Seite existert nicht.');
     }
   });
+});
+
+// Ändern eines Posts
+app.put('/post/:postid', function (req,res){
+
+  db.exists('Post:'+req.params.postid, function(err,rep){
+    if (rep){
+      var updatedPost = req.body;
+      updatedPost.id = req.params.postid;
+      updatedPost.LastEdited = date;
+
+      db.set('Post:'+req.params.postid, JSON.stringify(updatedPost), function(err, rep){
+        res.json(updatedPost);
+      });
+    }
+
+    else{
+        res.status(404).type('text').send('Der Post ist nicht vorhanden')
+    }
+  });
+});
+
+// Löschen eines Posts
+app.delete('/post/:postid',function(req, res){
+  db.del('Post:'+req.params.postid, function(err,rep){
+    if(rep){
+      res.status(200).type('text').send('ok');
+    }
+
+    else{
+      res.status(404).type('text').send('post nicht vorhanden');
+    }
+  })
 });
 
 //-------------------------------------------------------
@@ -48,16 +87,17 @@ app.get('/post/:id', function(req, res){
 app.post('/post/:postid/comment/', function(req, res){
 
   var newComment = req.body;
+  newComment.ErstelltAm = date;
 
   db.exists('Post:'+req.params.postid, function(err, rep){
 
     if(rep){
-      db.incr('CounterComments:OnPost'+req.params.postid, function(err, rep){
+      db.zincrby('Counter:OnPostCOMMENTS',1,'Post:'+req.params.postid, function(err, rep){
         cid = rep;
 
-        db.sadd('comments:post:'+req.params.postid, cid,function(err, rep){
+        db.sadd('Comments:post:'+req.params.postid, cid,function(err, rep){
 
-          db.hset('comments:'+req.params.postid, cid, JSON.stringify(newComment), function(err, rep){
+          db.hset('Comments:'+req.params.postid, cid, JSON.stringify(newComment), function(err, rep){
                   res.json(newComment);
           });
         });
@@ -71,7 +111,7 @@ app.post('/post/:postid/comment/', function(req, res){
 
 // Anfragen aller Kommentare zum Post
 app.get('/post/:id/comment/', function(req, res){
-  db.hgetall('comments:'+req.params.id, function(err, rep){
+  db.hgetall('Comments:'+req.params.id, function(err, rep){
     if(rep){
       res.type('json').send(rep);
     }
@@ -85,10 +125,10 @@ app.get('/post/:id/comment/', function(req, res){
 //Löschen eines Kommentare :cid aus dem Post :pid
 app.delete('/post/:pid/comment/:cid',function(req, res){
 
-    db.SREM('comments:post:'+req.params.pid, req.params.cid, function(err, rep){
+    db.SREM('Comments:post:'+req.params.pid, req.params.cid, function(err, rep){
       if(rep==0)res.status(400).type('text').send('klapptnicht');
       else{
-        db.HDEL('comments:'+req.params.pid, req.params.cid, function(err, rep){
+        db.HDEL('Comments:'+req.params.pid, req.params.cid, function(err, rep){
         res.status(200).type('text').send('Erfolgreich gelöscht!')
     });
     }
